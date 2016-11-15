6374b5b42daaf96ada3bc46e3d1528a611f9ab33
SIJI MOON <sijimoon@SIJI-no-MacBook-Air.local>
2016-11-15 16:30:49 +0900

    update image

--- GravityHP/ProductTableViewController.swift
+++ GravityHP/ProductTableViewController.swift
@@ -34,14 +34,14 @@ class ProductTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 7
+        return 1
     }
-
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
         // Configure the cell...
+        cell.imageView?.image = UIImage(named: "GravityHP-006")
 
         return cell
     }
